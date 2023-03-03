72d714c5c0018c89780d93cd6e15ccc2e7702e39
mqkotoo <87256037+mqkotoo@users.noreply.github.com>
2023-03-04 00:31:18 +0900

    テーマ切り替えスイッチの見た目変更

--- lib/presentation/search/widget/search_app_bar.dart
+++ lib/presentation/search/widget/search_app_bar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_switch/flutter_switch.dart';
 
 import '../../../generated/l10n.dart';
 import '../../../theme/theme_mode_provider.dart';
@@ -16,22 +17,46 @@ class SearchAppBar extends ConsumerWidget implements PreferredSizeWidget {
     //theme設定
     final themeMode = ref.watch(themeModeProvider);
     return AppBar(
-      title: Text(
-        S.of(context).searchPageTitle,
-        key: const Key("searchAppBar"),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            S.of(context).searchPageTitle,
+            key: const Key("searchAppBar"),
+          ),
+          FlutterSwitch(
+            value: themeMode == ThemeMode.dark,
+            onToggle: (value) {
+              final themeSelector = ref.read(themeModeProvider.notifier);
+              themeSelector.toggleThemeAndSave(value);
+            },
+            padding:0,
+            toggleSize: 30,
+            activeToggleColor: const Color(0xFF0D3B74),
+            inactiveToggleColor: const Color(0xFFFFAB40),
+            activeSwitchBorder: Border.all(
+              color: const Color(0x42000000),
+              width: 1.5,
+            ),
+            inactiveSwitchBorder: Border.all(
+              color: const Color(0x42000000),
+              width: 1.5,
+            ),
+
+            activeColor: const Color(0xFF365377),
+            inactiveColor: const Color(0xFFFFE892),
+
+            activeIcon: const Icon(
+              Icons.nightlight_round,
+              color: Colors.orangeAccent,
+            ),
+            inactiveIcon: const Icon(
+              Icons.wb_sunny,
+              color: Colors.white,
+            ),
+          )
+        ]
       ),
-      //APPBARの右側
-      actions: [
-        Switch(
-          //キャッシュされているモードがダークかで判定
-          value: themeMode == ThemeMode.dark,
-          activeColor: const Color(0xff64FFDA),
-          onChanged: (value) {
-            final themeSelector = ref.read(themeModeProvider.notifier);
-            themeSelector.toggleThemeAndSave(value);
-          },
-        )
-      ],
     );
   }
 }
