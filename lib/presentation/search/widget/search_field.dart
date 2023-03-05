cb5811d926f45371f367b3289e396836ab8951fd
mqkotoo <87256037+mqkotoo@users.noreply.github.com>
2023-03-05 23:40:36 +0900

    ソート用のUI作った

--- lib/presentation/search/widget/search_field.dart
+++ lib/presentation/search/widget/search_field.dart
@@ -34,51 +34,61 @@ class SearchFieldState extends ConsumerState<SearchField> {
     return SafeArea(
       top: false,
       bottom: false,
-      child: TextFormField(
-        key: const Key("inputForm"),
-        controller: textController,
-        onChanged: (text) {
-          ref
-              .read(isClearButtonVisibleProvider.notifier)
-              .update((state) => text.isNotEmpty);
-        },
-
-        //decoration
-        decoration: InputDecoration(
-          prefixIcon: const Icon(Icons.search, color: Colors.grey,size: 27,),
-          suffixIcon: ref.watch(isClearButtonVisibleProvider)
-              ? IconButton(
-              key: const Key("clearButton"),
-              icon: const Icon(Icons.clear,size: 27),
-              onPressed: () {
-                textController.clear();
+      child: Row(
+        children: [
+          Expanded(
+            child: TextFormField(
+              key: const Key("inputForm"),
+              controller: textController,
+              onChanged: (text) {
                 ref
-                    .watch(isClearButtonVisibleProvider.notifier)
-                    .update((state) => false);
+                    .read(isClearButtonVisibleProvider.notifier)
+                    .update((state) => text.isNotEmpty);
               },
-              color: Colors.grey)
-              : const SizedBox.shrink(),
-        ),
 
-        //入力キーボードのdone→searchに変更
-        textInputAction: TextInputAction.search,
-        //search押したらデータ取得
-        onFieldSubmitted: (text) async {
-          // エラーメッセージに値が入るかをエラー表示のフラグにしているから検索ごとに初期化
-          // android studioのバグなのか、refreshが使われていない判定になる
-          // ignore: unused_result
-          ref.refresh(errorMessageProvider);
+              //decoration
+              decoration: InputDecoration(
+                prefixIcon: const Icon(Icons.search, color: Colors.grey,size: 27),
+                suffixIcon: ref.watch(isClearButtonVisibleProvider)
+                    ? IconButton(
+                    key: const Key("clearButton"),
+                    icon: const Icon(Icons.clear,size: 27),
+                    onPressed: () {
+                      textController.clear();
+                      ref
+                          .watch(isClearButtonVisibleProvider.notifier)
+                          .update((state) => false);
+                    },
+                    color: Colors.grey)
+                    : const SizedBox.shrink(),
+              ),
 
-          final connectivityResult = await connectivity.checkConnectivity();
-          //通信がなかったら何もその後の処理はせず、エラーを出す
-          if (connectivityResult == ConnectivityResult.none) {
-            ref
-                .read(errorMessageProvider.notifier)
-                .update((state) => S.of(context).networkError);
-            return;
-          }
-          ref.read(inputRepoNameProvider.notifier).update((state) => text);
-        },
+              //入力キーボードのdone→searchに変更
+              textInputAction: TextInputAction.search,
+              //search押したらデータ取得
+              onFieldSubmitted: (text) async {
+                // エラーメッセージに値が入るかをエラー表示のフラグにしているから検索ごとに初期化
+                // android studioのバグなのか、refreshが使われていない判定になる
+                // ignore: unused_result
+                ref.refresh(errorMessageProvider);
+
+                final connectivityResult = await connectivity.checkConnectivity();
+                //通信がなかったら何もその後の処理はせず、エラーを出す
+                if (connectivityResult == ConnectivityResult.none) {
+                  ref
+                      .read(errorMessageProvider.notifier)
+                      .update((state) => S.of(context).networkError);
+                  return;
+                }
+                ref.read(inputRepoNameProvider.notifier).update((state) => text);
+              },
+            ),
+          ),
+          IconButton(
+              onPressed: (){},
+              icon: const Icon(Icons.sort,size: 35),
+          )
+        ],
       ),
     ); // ここにウィジェットツリーを構築するコードを記述してください。
   }
