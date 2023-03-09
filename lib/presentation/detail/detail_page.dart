40d71287091284c48f2a80eb65591d0565d943f3
mqkotoo <87256037+mqkotoo@users.noreply.github.com>
2023-03-09 22:15:37 +0900

    前回コミットの漏れ対応

--- lib/presentation/detail/detail_page.dart
+++ lib/presentation/detail/detail_page.dart
@@ -97,22 +97,7 @@ class DetailPage extends StatelessWidget {
                 ),
                 const SizedBox(height: 30),
                 //githubに飛ばす
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: GestureDetector(
-                    onTap: () => _openGitHubUrl(Uri.parse(repoData.htmlUrl)),
-                    child: Text(
-                      S.of(context).viewOnGitHub,
-                      style: const TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.blueAccent,
-                        decoration: TextDecoration.underline,
-                        decorationColor: Colors.blueAccent,
-                      ),
-                    ),
-                  ),
-                ),
+                githubLinkText(context),
                 const SizedBox(height: 30),
               ],
             ),
