14dbcab69c0dc4c96e67a554601380e49374531b
mqkotoo <87256037+mqkotoo@users.noreply.github.com>
2023-03-10 15:57:27 +0900

    統合テスト成功するように改善

--- integration_test/search_test.dart
+++ integration_test/search_test.dart
@@ -1,11 +1,15 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:github_search_study/main.dart';
+import 'package:github_search_study/generated/l10n.dart';
+import 'package:github_search_study/presentation/search/search_page.dart';
 import 'package:github_search_study/repository/providers/http_client.dart';
+import 'package:github_search_study/theme/shared_preferences.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:network_image_mock/network_image_mock.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../test/repository/repository_mock_data.dart';
 import '../test/repository/repository_mock_test.mocks.dart';
@@ -14,6 +18,23 @@ import 'package:http/http.dart' as http;
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
+  //英語でのWIDGETテストなので英語設定のアプリを使ってテストする
+  Widget myTestWidget() {
+    return MaterialApp(
+      //多言語対応
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      //言語を英語に設定する
+      locale: const Locale('en'),
+      home: const SearchPage(),
+    );
+  }
+
   testWidgets('検索と画面遷移の統合テスト', (tester) async {
     const data = RepositoryMockData.jsonData;
     final mockClient = MockClient();
@@ -21,9 +42,13 @@ void main() {
 
     await mockNetworkImagesFor(() async {
       await tester.pumpWidget(
-        ProviderScope(
-            overrides: [httpClientProvider.overrideWithValue(mockClient)],
-            child: const MyApp()),
+        ProviderScope(overrides: [
+          httpClientProvider.overrideWithValue(mockClient),
+          // sharedPreferencesインスタンス化
+          sharedPreferencesProvider.overrideWithValue(
+            await SharedPreferences.getInstance(),
+          ),
+        ], child: myTestWidget()),
       );
 
       expect(find.byKey(const Key('searchAppBar')), findsOneWidget);
